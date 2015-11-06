b3f3ba923d69bf37d94b2496752be67c64d505ac
LuckyJS <charismajs@gmail.com>
2015-11-06 18:16:36 +0900

    try to decode image of barcode

--- public/main/main.js
+++ public/main/main.js
@@ -68,74 +68,6 @@
 
     function attachListeners() {
       $("#barcodeFileInput").on("change", vm.dropChangeHandler);
-      //document.getElementById("barcodeFileInput").onchange = function(e) {
-      //  var file = e.target.files[0];
-      //  var canvas = document.getElementById("picture");
-      //  var ctx = canvas.getContext("2d");
-      //  var cx = 0, cy = 0;
-      //
-      //  var image = document.getElementById("picturePreview");
-      //  var URL = window.URL || window.webkitURL;
-      //  var imageURL = URL.createObjectURL(file);
-      //  image.setAttribute("src", imageURL);
-      //
-      //  var cw = image.width, ch = image.height;
-      //  var orientation = 1;
-      //
-      //  loadImage.parseMetaData(
-      //    file,
-      //    function(data) {
-      //      if (!data.imageHead) return;
-      //
-      //      orientation = data.exif.get("Orientation");
-      //      var imageWidth = data.exif.get("ImageWidth");
-      //      var imageHeight = data.exif.get("ImageHeight");
-      //
-      //      console.log("Orientation : ", orientation);
-      //
-      //      switch (orientation) {
-      //        case 8:
-      //          ctx.rotate(90 * Math.PI / 180);
-      //          cw = image.height;
-      //          ch = image.width;
-      //          cy = image.height * (-1);
-      //          break;
-      //        case 3:
-      //          ctx.rotate(180 * Math.PI / 180);
-      //          cx = image.width * (-1);
-      //          cy = image.height * (-1);
-      //          break;
-      //        case 6:
-      //          ctx.rotate(-90 * Math.PI / 180);
-      //          cw = image.height;
-      //          ch = image.width;
-      //          cx = image.width * (-1);
-      //          break;
-      //      }
-      //    },
-      //    {
-      //      maxMetaDataSize: 262144,
-      //      disableImageHead: false
-      //    }
-      //  );
-      //
-      //  loadImage(
-      //    file,
-      //    function(img) {
-      //      //document.body.appendChild(img);
-      //      canvas.setAttribute('width', cw);
-      //      canvas.setAttribute('height', ch);
-      //      ctx.drawImage(img, cx,cy);
-      //    },
-      //    {
-      //      maxWidth: 500,
-      //      minWidth: 100,
-      //      orientation: orientation,
-      //      canvas: true
-      //    }
-      //  );
-      //
-      //};
 
       $("#startDecode").on("click", function(e) {
         var input = $("#barcodeFileInput");
@@ -144,61 +76,7 @@
         }
         decode(tmpImageUrl);
       });
-
-      //$("#barcodeFileInput").on("change", function(e) {
-      //  $("#codeResult").text(" ");
-      //  var files = e.target.files, file;
-      //  var canvas = document.getElementById("picture");
-      //  var ctx = canvas.getContext("2d");
-      //
-      //
-      //  if (files && files.length > 0) {
-      //    file = files[0];
-      //    try {
-      //      var URL = window.URL || window.webkitURL;
-      //      var imageURL = URL.createObjectURL(file);
-      //      $("#picturePreview").attr("src", imageURL);
-      //    }
-      //    catch(e) {
-      //      try {
-      //        var fileReader = new FileReader();
-      //
-      //        fileReader.onload = function(event) {
-      //          $("#picturePreview").attr("src", event.target.result);
-      //        };
-      //
-      //        fileReader.onloadend = function(event) {
-      //          var exif = EXIF.readFromBinaryFile(new BinaryFile(event.target.result));
-      //          console.log("EXIF : ", exif);
-      //          $("#debug").text(exif.orientation);
-      //
-      //          switch(exif.Orientation){
-      //            case 8:
-      //              ctx.rotate(90 * Math.PI / 180);
-      //              break;
-      //            case 3:
-      //              ctx.rotate(180 * Math.PI / 180);
-      //              break;
-      //            case 6:
-      //              ctx.rotate(-90 * Math.PI / 180);
-      //              break;
-      //          }
-      //        };
-      //
-      //        fileReader.readAsDataURL(file);
-      //      }
-      //      catch(err) {
-      //        $("#codeResult").text("Fail! createObjectURL and FileReader are not supported by browser");
-      //      }
-      //    }
-      //  }
-      //
-      //});
     }
-
-    //function detachListeners() {
-    //  $("#startDecode").off("click");
-    //}
 
     function decode(src) {
       console.log("Decode");
@@ -206,9 +84,10 @@
 
       Quagga.decodeSingle({
         inputStream: {
-          size: 640,
+          //size: 640,
           singleChannel: false
         },
+        tracking: true,
         locator: {
           patchSize: "large",
           halfSample: false
