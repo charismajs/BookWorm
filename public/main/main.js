92317424b3bbd369d9cb37f8ccae8b340e2d6c80
LuckyJS <charismajs@gmail.com>
2015-11-09 10:52:35 +0900

    try to decode image of barcode

--- public/main/main.js
+++ public/main/main.js
@@ -12,18 +12,47 @@
 
   function MainCtrl() {
     var vm = this;
+    var image;
 
     var picture = $("#picture");
     //var picturePreview = document.getElementById("picturePreview");
     //var currentFile;
 
+    var config = {
+      "default": {
+        inputStream: {
+          name: "Test",
+          type: "ImageStream",
+          length: 10,
+          size: 800
+        },
+        locator: {
+          patchSize: "medium",
+          halfSample: true
+        }
+      },
+      "i2of5_reader": {
+        inputStream: {
+          size: 800,
+          type: "ImageStream",
+          length: 5
+        },
+        locator: {
+          patchSize: "small",
+          halfSample: false
+        }
+      }
+    };
+
     vm.replaceResult = function(img) {
       var content;
+      image = img;
+
       if (!(img.src || img instanceof HTMLCanvasElement)) {
         content = $("<span>Loading image file failed</span>")
       } else {
         //picturePreview.setAttribute("src", img.src || img.toDataURL());
-        content = $("<img>").append().attr("src", img.src || img.toDataURL());
+        content = $("<img>").append().attr("src", img.src || img.toDataURL()).attr("id", "preview");
       }
       picture.children().replaceWith(content);
     };
@@ -64,6 +93,7 @@
 
     function init() {
       attachListeners();
+      //quaggaInit();
     }
 
     function attachListeners() {
@@ -71,10 +101,16 @@
 
       $("#startDecode").on("click", function(e) {
         var input = $("#barcodeFileInput");
-        if (input[0].files && input[0].files.length) {
-          var tmpImageUrl = URL.createObjectURL(input[0].files[0]);
+
+        //if (input[0].files && input[0].files.length) {
+        //  var tmpImageUrl = URL.createObjectURL(input[0].files[0]);
+        //}
+        var img = document.getElementById("preview");
+        if (img) {
+          var tmpImageUrl = img.getAttribute("src");
         }
         decode(tmpImageUrl);
+        //start();
       });
     }
 
@@ -84,18 +120,20 @@
 
       Quagga.decodeSingle({
         inputStream: {
-          //size: 640,
-          singleChannel: false
+          size: 1920
+          //singleChannel: false
         },
-        tracking: true,
+        ////tracking: true,
         locator: {
-          patchSize: "medium",
-          halfSample: false
+          patchSize: "small",
+          halfSample: true
         },
         decoder: {
-          readers: ["upc_reader", "code_128_reader", "code_39_reader", "code_39_vin_reader", "ean_8_reader", "ean_reader", "upc_e_reader", "codabar_reader"]
+          //readers: ["ean_reader", "upc_reader", "code_128_reader", "code_39_reader", "code_39_vin_reader", "ean_8_reader", "upc_e_reader", "codabar_reader"]
+          readers: ["ean_reader"]
         },
         locate: true,
+        numOfWorkers: 4,
         //visual: true,
         src: src
       }, function(result) {
@@ -105,8 +143,53 @@
         else {
           $("#codeResult").text("unable to read");
         }
-      })
+      });
     }
+
+    function quaggaInit() {
+      Quagga.init(config.default, function() {
+        start();
+      });
+    }
+
+    function start() {
+      Quagga.start();
+    }
+
+    Quagga.onProcessed(function(result) {
+      var drawingCtx = Quagga.canvas.ctx.overlay,
+        drawingCanvas = Quagga.canvas.dom.overlay;
+
+      if (result) {
+        if (result.boxes) {
+          drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+          result.boxes.filter(function (box) {
+            return box !== result.box;
+          }).forEach(function (box) {
+            Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+          });
+        }
+
+        if (result.box) {
+          Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+          Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+        }
+      }
+    });
+
+    Quagga.onDetected(function(result) {
+      var $node,
+        canvas = Quagga.canvas.dom.image,
+        detectedCode = result.codeResult.code;
+
+      $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
+      $node.find("img").attr("src", canvas.toDataURL());
+      $node.find("h4.code").html(detectedCode);
+      $("#result_strip ul.thumbnails").prepend($node);
+    });
 
 
     //function stop() {
